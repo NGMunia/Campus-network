d465d6bbff61891942e4de7458c6fddc4dced12a
NGMunia <anthony.ngure@yahoo.com>
2023-11-11 10:13:32 +0300

    projects

---
+++ Network/Devices.py
@@ -0,0 +1,153 @@
+
+Firewall_A_10= {
+         'FW1-A-10':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'192.168.11.1'
+                    }
+                }
+Spokes  =   {
+            'NBI-1':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.54.1',
+                      'timeout': 15
+                    },
+            'NBI-2':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.54.2'
+                    },
+              'NKR':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.52.1'
+                    },
+              'MSA':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.60.1'
+                    },
+              'MLD':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.61.1'
+                    },
+              # 'KSM':{
+              #         'device_type':'cisco_ios',
+              #         'username': 'Automation',
+              #         'secret': 'cisco123',
+              #         'password': 'cisco123',
+              #         'ip':'10.1.51.6'
+          #           # }
+          'KSM-LAN':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.70.1'
+                    }                  
+          }
+
+Area_0 =  {
+          'R1-A-0': {
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.0.1'
+                    },
+          'R2-A-0': {
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.0.2'
+                    },  
+          'R3-A-0': {
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.0.3'
+                    }, 
+          'R4-A-0': {
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.0.4'
+                    }
+          }
+
+Area_10 = {
+          'R5-A-10':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.10.2',
+                      'ip':'10.1.10.6'
+                    },
+          'R6-A-10':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'10.1.10.10'
+                    },  
+          'R7-A-10':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'172.20.20.1',
+                      'ip':'172.20.21.1'
+                    }
+          }
+
+Area_23 = {
+          'R1-A-23':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'192.168.2.1',
+                    },
+          'R2-A-23':{
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'192.168.2.2'
+                    }
+         }
+Firewalls_A_51 =    {
+          'FW-1': {
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'172.16.0.1',
+                    },
+          'FW-2':  {
+                      'device_type':'cisco_ios',
+                      'username': 'Automation',
+                      'secret': 'cisco123',
+                      'password': 'cisco123',
+                      'ip':'172.17.0.1'
+                    }
+          }
+ 
